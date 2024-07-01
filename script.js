ccffeea216e8ae339e811b88167e3047e2683073
Isungrai3 <raiisung9@gmail.com>
2024-07-01 15:13:44 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//var bottle="pani"
+//let bottle='pani'
+//const bottle='pani' 
+
+const friend1 ='pk'
+const friend2 = 'jk'
+const friend3 = 'kk'
+
+const frinds = ['pk','jk','kk',5,true]
+
+const name = "isung rai"
+const age = 20
+const address = 'dharan'
+
+const me ={
+    name :'isung rai',
+    age : 20,
+    address : 'dharan'
+}
